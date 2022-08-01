8ccac32498be7066a595fdc758bfea8523118d36
Cl0ck <62774712+Cl0ckHvH@users.noreply.github.com>
2022-08-01 16:44:56 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,33 @@
+import toml
+import os
+import random
+
+from vkbottle import API, user
+from vkbottle.user import User, Message, run_multibot
+
+with open("config.toml", "r", encoding="utf-8") as f:
+    if "token1" in os.environ and "token2" in os.environ:
+        config = dict(os.environ)
+        for key, value in toml.load(f).items():
+            if key not in config:
+                config[key] = value
+    else:
+        config = toml.load(f)
+
+user = User()
+
+@user.on.message(action = "chat_kick_user")
+async def send_message(message: Message):
+    if message.peer_id == 2000000000 + int(config["chat_id1"]):
+        await message.ctx_api.messages.add_chat_user(
+           chat_id=int(config["chat_id1"]),
+           user_id=int(config["user_id1"])
+        )
+    elif message.peer_id == 2000000000 + int(config["chat_id2"]):
+        await message.ctx_api.messages.add_chat_user(
+            chat_id=int(config["chat_id2"]),
+            user_id=int(config["user_id2"])
+        )
+
+if __name__ == "__main__":
+    run_multibot(user, apis=(API(config["token1"]), API(config["token2"])))
