7f249bf52345c61390ff3c807d62c299b7114f7d
Cl0ck <62774712+Cl0ckHvH@users.noreply.github.com>
2022-08-10 21:40:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import random
 
 from vkbottle import API, user
 from vkbottle.user import User, Message, run_multibot
+from vkbottle.exception_factory import VKAPIError
 
 with open("config.toml", "r", encoding="utf-8") as f:
     if "token1" in os.environ and "token2" in os.environ:
@@ -18,16 +19,21 @@ user = User()
 
 @user.on.message(action = "chat_kick_user")
 async def send_message(message: Message):
-    if message.peer_id == 2000000000 + int(config["chat_id1"]):
-        await message.ctx_api.messages.add_chat_user(
-           chat_id=int(config["chat_id1"]),
-           user_id=int(config["user_id1"])
-        )
-    elif message.peer_id == 2000000000 + int(config["chat_id2"]):
-        await message.ctx_api.messages.add_chat_user(
-            chat_id=int(config["chat_id2"]),
-            user_id=int(config["user_id2"])
-        )
+    try:
+        if message.peer_id == 2000000000 + int(config["chat_id1"]):
+            await message.ctx_api.messages.add_chat_user(
+            chat_id=int(config["chat_id1"]),
+            user_id=int(config["user_id1"])
+            )
+        elif message.peer_id == 2000000000 + int(config["chat_id2"]):
+            await message.ctx_api.messages.add_chat_user(
+                chat_id=int(config["chat_id2"]),
+                user_id=int(config["user_id2"])
+            )
+    except VKAPIError[14]:
+        await send_message(message)
+    except VKAPIError:
+        print("")
 
 if __name__ == "__main__":
     run_multibot(user, apis=(API(config["token1"]), API(config["token2"])))
