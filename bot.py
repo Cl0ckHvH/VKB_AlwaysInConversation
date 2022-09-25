42476da34d8b7fd31a69922917fea0988be75995
Cl0ck <62774712+Cl0ckHvH@users.noreply.github.com>
2022-09-25 14:44:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import toml
 import os
-import random
+import asyncio
 
-from vkbottle import API, user
-from vkbottle.user import User, Message, run_multibot
+from vkbottle import API
 from vkbottle.exception_factory import VKAPIError
+from vkbottle.user import User, Message, run_multibot
 
 with open("config.toml", "r", encoding="utf-8") as f:
-    if "token1" in os.environ and "token2" in os.environ:
+    if "token" in os.environ:
         config = dict(os.environ)
         for key, value in toml.load(f).items():
             if key not in config:
@@ -16,24 +16,33 @@ with open("config.toml", "r", encoding="utf-8") as f:
         config = toml.load(f)
 
 user = User()
+member_id_list = []
 
-@user.on.message(action = "chat_kick_user")
-async def send_message(message: Message):
-    try:
-        if message.peer_id == 2000000000 + int(config["chat_id1"]):
-            await message.ctx_api.messages.add_chat_user(
-            chat_id=int(config["chat_id1"]),
-            user_id=int(config["user_id1"])
-            )
-        elif message.peer_id == 2000000000 + int(config["chat_id2"]):
-            await message.ctx_api.messages.add_chat_user(
-                chat_id=int(config["chat_id2"]),
-                user_id=int(config["user_id2"])
-            )
-    except VKAPIError[14]:
-        await send_message(message)
-    except VKAPIError:
-        print("")
+@user.on.chat_message(action = "chat_kick_user")
+async def add_chat_user(message: Message):
+    for i in range(0, len(member_id_list)):
+        try:
+            await message.ctx_api.messages.add_chat_user(chat_id=(message.peer_id - 2000000000), user_id=member_id_list[i])
+            break
+        except VKAPIError[15]:
+            pass
+        except VKAPIError[9]:
+            print("Flood control")
+            break
+        except VKAPIError as e:
+            print(e)
+            pass
+
+############################### Настройка перед запуском ######################################################
+
+async def api_from_config(apies):
+    global member_id_list
+    for i in range(0, len(config["token"])):
+        apies.append(API(config["token"][i]))
+        member_id_list.append(int(dict(await User(config["token"][i]).api.account.get_profile_info())['id']))
 
 if __name__ == "__main__":
-    run_multibot(user, apis=(API(config["token1"]), API(config["token2"])))
+    apies = []
+    api_climer = asyncio.get_event_loop()
+    api_climer.run_until_complete(api_from_config(apies))
+    run_multibot(user, apis=(apies))
